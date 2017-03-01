8f41663dd009555ad62c6deafe1a823195fc987b
root <root@ubuntu>
2017-03-01 02:47:32 -0800

    fix sensiolabs feedback

--- DependencyInjection/SmileEzEESectionFormFieldExtension.php
+++ DependencyInjection/SmileEzEESectionFormFieldExtension.php
@@ -2,12 +2,10 @@
 
 namespace Smile\EzEESectionFormFieldBundle\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Yaml\Yaml;
 
 class SmileEzEESectionFormFieldExtension extends Extension implements PrependExtensionInterface
